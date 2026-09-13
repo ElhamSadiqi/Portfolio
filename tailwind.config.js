405168eb40a050cf319455bbe2486ea46b4db178
Elham Sadiqi <sadiqielham44@gmail.com>
2026-09-13 20:14:27 +0430

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // toggled via 'dark' class on body
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  
+  plugins: [],
+}
